2c16c4128db080eccd83f8fec7b47d0ef0872238
Sulton Wibawa <msulton55@gmail.com>
2020-10-31 13:45:21 +0700

    changing sidebar layout

--- js/user-script.js
+++ js/user-script.js
@@ -1,32 +1,32 @@
-function CardCollection() {
-  const container = document.getElementById("card-collection")
-  let totalCards = container.childElementCount
+function Main() {
+
+  const container1 = document.getElementById("card-collection")
+  const container2 = document.getElementById("page-top")
+  let totalCards = container1.childElementCount
   let currentScreenWidth = window.screen.width
   let mobileScreenWidth = 0
 
   if (currentScreenWidth < 575.98) {
-    container.style.removeProperty("width")
-      container.style.removeProperty("flex-wrap")
+    container1.style.removeProperty("width")
+    container1.style.removeProperty("flex-wrap")
+    container2.style.removeProperty("overflow")
   } else {
-    container.style.width = `calc((100vw * ${totalCards}) - (120% * ${totalCards}))`
+    container1.style.width = `calc((100vw * ${totalCards}) - (120% * ${totalCards}))`
   }
 
   window.addEventListener("resize", (event) => {
     mobileScreenWidth = window.screen.width
     console.log(mobileScreenWidth)
     if (mobileScreenWidth < 575.98) {
-      container.style.removeProperty("width")
-      container.style.removeProperty("flex-wrap")
+      container1.style.removeProperty("width")
+      container1.style.removeProperty("flex-wrap")
+      container2.style.removeProperty("overflow")
     } else {
-      container.style.width = `calc((100vw * ${totalCards}) - (120% * ${totalCards}))`
-      container.style.flexWrap =  `nowrap`
+      container1.style.width = `calc((100vw * ${totalCards}) - (120% * ${totalCards}))`
+      container1.style.flexWrap =  `nowrap`
+      container2.style.overflow = "hidden"
     }
   })
-}
-
-function Main() {
-
-  CardCollection()
 
 }
 
